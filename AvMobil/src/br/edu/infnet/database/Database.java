3d03431badc517198355b60081f1409c0c42a8ed
Welinton R Gonçalves <welinton.goncalves@al.infnet.edu.br>
2015-02-21 19:30:51 -0200

    Correção da Classe Database.

---
+++ AvMobil/src/br/edu/infnet/database/Database.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.database;
+
+public class Database {
+		
+	static String serverName = "localhost"; 
+	static String database = "mysql"; 
+	static String url = "jdbc:mysql://" + serverName + "/" + database; 
+	static String username = "root";  
+	static String password = "admin";
+	
+}
+

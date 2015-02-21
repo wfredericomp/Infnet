26b992cea3fc9b15b8ebc77b5a8ebcb30661d991
Welinton R Gonçalves <welinton.goncalves@al.infnet.edu.br>
2015-02-21 21:04:42 -0200

    Criado o pacote br.edu.infnet.control contendo a Classe control; Alterações diversas na classe Console;

--- AvMobil/src/br/edu/infnet/console/Console.java
+++ AvMobil/src/br/edu/infnet/console/Console.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.sql.Statement;
+import java.text.NumberFormat;
 
 import br.edu.infnet.model.*;
 import br.edu.infnet.database.*;
@@ -14,6 +15,8 @@ public class Console {
 		// TODO Auto-generated method stub
 		
 	   ArrayList<Carro> carrosLista = new ArrayList<Carro>();
+	   NumberFormat formatoMoeda = NumberFormat.getCurrencyInstance();   
+	   
 		           
        try {     
     	   Statement st = Conexao.getConexao().createStatement();
@@ -36,16 +39,26 @@ public class Console {
        } catch (SQLException e) {
             	return;
        	 }
- 
+       
   		System.out.println("#######################################################");
    		System.out.println("#######          BEM VINDO A AV MOBIL          ########");
    		System.out.println("#######################################################");
        
-   		System.out.println("Estes são nossos carros disponíveis:");
+   		System.out.println("\n Estes são nossos carros disponíveis: \n");
    		
    		
             for(int i=0; i<carrosLista.size();i++) {
-            	System.out.println(i + " - Nome: " + (carrosLista.get(i)).getModelo());
+
+            	System.out.println((i+1) + " - Modelo: " + (carrosLista.get(i)).getModelo().toUpperCase());
+            	System.out.println("    Montadora: " + (carrosLista.get(i)).getMontadora().toUpperCase());
+            	System.out.println("    Tipo: " + (carrosLista.get(i)).getTipo().toUpperCase());
+            	System.out.println("    Cor: " + (carrosLista.get(i)).getCor().toUpperCase());
+            	System.out.println("    Câmbio: " + (carrosLista.get(i)).getCambio().toUpperCase());
+            	System.out.println("    Motor: " + (carrosLista.get(i)).getMotor().toUpperCase());
+            	System.out.println("    Preço: " + formatoMoeda.format((carrosLista.get(i)).getPreco()));
+
+            	System.out.println("-------------------------------------------------");
+
             }
         		
 	}
---
+++ AvMobil/src/br/edu/infnet/control/Control.java
@@ -0,0 +1,5 @@
+package br.edu.infnet.control;
+
+public class Control {
+
+}

ac379d6ac557d5fb8ce3c8b1a75c658c83d17d06
Waldivino Frederico <waldivino.marques@al.infnet.edu.br>
2015-02-21 17:29:57 -0200

    Criada atributos, construtores e gt e sets da classe Loja.

--- AvMobil/src/br/edu/infnet/model/Loja.java
+++ AvMobil/src/br/edu/infnet/model/Loja.java
@@ -1,5 +1,56 @@
 package br.edu.infnet.model;
 
+import java.util.ArrayList;
+
 public class Loja {
+	private String nome;
+	private String endereco;
+	private ArrayList<Carro> listCar = new ArrayList<Carro>();
+	private ArrayList<Motocicleta> listMotocicleta = new ArrayList<Motocicleta>();
+
+	public Loja() {
+		super();
+	}
+
+	public Loja(String nome, String endereco, ArrayList<Carro> listCar,
+			ArrayList<Motocicleta> listMotocicleta) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.listCar = listCar;
+		this.listMotocicleta = listMotocicleta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public ArrayList<Carro> getListCar() {
+		return listCar;
+	}
+
+	public void setListCar(Carro listCar) {
+		this.listCar.add(listCar);
+	}
+
+	public ArrayList<Motocicleta> getListMotocicleta() {
+		return listMotocicleta;
+	}
+
+	public void setListMotocicleta(Motocicleta listMotocicleta) {
+		this.listMotocicleta.add(listMotocicleta);
+	}
 
 }

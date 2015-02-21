5345d05fa1bb838071a19c6472e9d40635e93e12
Welinton R Gonçalves <welinton.goncalves@al.infnet.edu.br>
2015-02-21 18:55:49 -0200

    Altearada a Classe Database.

--- AvMobil/src/br/edu/infnet/database/Database.java
+++ AvMobil/src/br/edu/infnet/database/Database.java
@@ -1,5 +1,12 @@
 package br.edu.infnet.database;
 
 public class Database {
+		
+	String serverName = "localhost"; 
+	String database = "mysql"; 
+	String url = "jdbc:mysql://" + serverName + "/" + database; 
+	String username = "root";  
+	String password = "admin";
 
 }
+

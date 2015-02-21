7306e1c47e38bdf121a1acb121a10d86671fd24f
Welinton R Gonçalves <welinton.goncalves@al.infnet.edu.br>
2015-02-21 19:56:20 -0200

    Correção do atributo database da classe Database;

--- AvMobil/src/br/edu/infnet/database/Database.java
+++ AvMobil/src/br/edu/infnet/database/Database.java
@@ -3,7 +3,7 @@ package br.edu.infnet.database;
 public class Database {
 		
 	static String serverName = "localhost"; 
-	static String database = "mysql"; 
+	static String database = "avmobildb"; 
 	static String url = "jdbc:mysql://" + serverName + ":3306/" + database; 
 	static String username = "root";  
 	static String password = "admin";

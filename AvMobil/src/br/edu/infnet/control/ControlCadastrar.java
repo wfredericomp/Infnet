75854ac51b3a9b6b712774b737cbd1f2db2e80c0
Welinton R Gonçalves <welinton.goncalves@al.infnet.edu.br>
2015-02-23 11:43:42 -0300

    Complemento metodo cadastrarVeiculo;

--- AvMobil/src/br/edu/infnet/control/ControlCadastrar.java
+++ AvMobil/src/br/edu/infnet/control/ControlCadastrar.java
@@ -15,21 +15,57 @@ public class ControlCadastrar {
 	
 		sc = new Scanner(System.in);
 		String chassi;
+		String montadora;
+		String modelo;
+		String tipo;
+		String cor;
+		String motor;
+		String cambio;
+		float preco;
+		
 		System.out.println("Informe os dados do veículo:");
 		
 		System.out.println("Chassi:");
 		chassi = sc.next();
-		sc.close();
-					
+		
+		System.out.println("Montadora:");
+		montadora = sc.nextLine();
+		
+		System.out.println("Modelo:");
+		modelo = sc.nextLine();
+		
+		System.out.println("Tipo:");
+		tipo = sc.nextLine();
+		
+		System.out.println("Cor:");
+		cor = sc.nextLine();
+		
+		System.out.println("Motor:");
+		motor = sc.nextLine();
+		
+		System.out.println("Câmbio:");
+		cambio = sc.nextLine();
+		
+		System.out.println("Preço:");
+		preco = sc.nextFloat();
+		
 		try {
 			
 			Connection con = Conexao.getConexao();
 			
-	    	String sql = "INSERT INTO carro (chassi) VALUES (?)"; 
+	    	String sql = "INSERT INTO carro (chassi, montadora, modelo, tipo, cor, motor, cambio, preco )" +
+	    	"VALUES (?,?,?,?,?,?,?,?)"; 
 	    	    	
 	    	PreparedStatement comando = con.prepareStatement(sql);
 			
-	    	comando.setString(1, chassi);
+	    	comando.setString(1, chassi.toUpperCase());
+	    	comando.setString(2, montadora.toUpperCase());
+	    	comando.setString(3, modelo.toUpperCase());
+	    	comando.setString(4, tipo.toUpperCase());
+	    	comando.setString(5, cor.toUpperCase());
+	    	comando.setString(6, motor.toUpperCase());
+	    	comando.setString(7, cambio.toUpperCase());
+	    	comando.setFloat(8, preco);
 	    	comando.executeUpdate();    	
 	    	
 		} catch (SQLException e) {
